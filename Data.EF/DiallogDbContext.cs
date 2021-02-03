6f60784b243fbcbde93b77b56a9f2cc30e849b7c
Van Labajo <van.labajo@serialmultivision.com>
2021-02-03 23:46:11 +0800

    Added partial data layer project

---
+++ Data.EF/DiallogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.EF.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EF
+{
+    public class DiallogDbContext : DbContext
+    {
+        public DbSet<RoleToPermissions> RolesToPermissions { get; set; }
+
+        public DiallogDbContext(DbContextOptions<DiallogDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new RoleToPermissionsEntityConfiguration());
+        }
+    }
+}
---
+++ Data.EF/EntityConfigurations/RoleToPermissionsEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.EF.EntityConfigurations
+{
+    class RoleToPermissionsEntityConfiguration : IEntityTypeConfiguration<RoleToPermissions>
+    {
+        public void Configure(EntityTypeBuilder<RoleToPermissions> builder)
+        {
+            builder.Property("_permissionsInRole")
+                .HasColumnName("PermissionsInRole");
+        }
+    }
+}
